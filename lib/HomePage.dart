24e7c6159e0b6024a0c12baf71199c2aad347754
Kevin <bubbagump2020@gmail.com>
2020-06-23 15:36:50 -0500

    Login and Sign Up
    Getting the routes for the Login and Sign Up pages going

---
+++ lib/HomePage.dart
@@ -0,0 +1,55 @@
+import 'package:diabetichabits/SignUp.dart';
+import 'package:flutter/material.dart';
+import 'package:diabetichabits/LogIn.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context){
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Column(
+        children: <Widget>[
+         Padding(
+           padding: const EdgeInsets.symmetric(vertical: 50.0),
+           child: SizedBox(
+             height: 50.0,
+             width: double.infinity,
+             child: RaisedButton(
+               color: Colors.blue,
+               textColor: Colors.white,
+               onPressed: () {
+                 Navigator.push(
+                   context,
+                   MaterialPageRoute(
+                     builder: (context) => SignUpPage(),
+                   ),
+                 );
+               },
+               child: Text('Sign Up', style: TextStyle(fontSize: 25))
+             ),
+           ),
+         ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50.0),
+            child: SizedBox(
+              height: 50.0,
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(),
+                    ),
+                  );
+                },
+                child: Text('Log In', style: TextStyle(fontSize: 25))
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:diabetichabits/SignUp.dart';
+import 'package:diabetichabits/HomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: SignUpPage(),
+        body: HomePage(),
       ),
     );
   }
